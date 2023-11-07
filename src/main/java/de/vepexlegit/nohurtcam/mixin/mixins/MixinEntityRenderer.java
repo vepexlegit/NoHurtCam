3cd2f97c64306a163ad28e252daa1bd4a00b8555
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-11-07 09:52:00 +0400

    Create MixinEntityRenderer.java

---
+++ src/main/java/de/vepexlegit/nohurtcam/mixin/mixins/MixinEntityRenderer.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.nohurtcam.mixin.mixins;
+
+import net.minecraft.client.renderer.EntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer {
+    @Inject(method = "hurtCameraEffect", at = @At("HEAD"), cancellable = true)
+    private void onHurtCameraEffect(final float partialTicks, final CallbackInfo ci) {
+        ci.cancel();
+    }
+}
